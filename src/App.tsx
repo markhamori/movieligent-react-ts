18ec1adce41e3b063562e65ac40cff51a5f2fd2f
Márk Hámori <hamorimark@hotmail.com>
2022-07-13 20:42:29 +0200

    Figma design, test with dummy data from movies api

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 // Components
-import { Header } from "./components/Header/Header"
-import { Main } from "./components/Main/Main"
-import { Footer } from "./components/Footer/Footer"
-
-// Styles
-import './App.css';
+import { Header } from "./components/Header/Header";
+import { Main } from "./components/Main/Main";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="app">
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,24 @@
-import "./Main.css"
+import { useEffect, useState } from "react";
+import dummyData from "../../dummyData.json";
+
+import "./Main.css";
 
 export const Main = () => {
+  const [movies, setMovies] = useState<String[] | undefined>([]);
+
+  const fetchDummyData = () => {
+    return dummyData.results.map((res) => setMovies([res.original_title]));
+  };
+
+  useEffect(() => {
+    fetchDummyData();
+  }, []);
+
   return (
     <div>
       <p>Main</p>
       <button role="alert">Alert</button>
+      {movies && movies.map((movie) => <p>{movie}</p>)}
     </div>
-  )
-}
+  );
+};
