4a98fe1642a3b0406b5f38b14cc2e35747c95f90
Hámori Márk <hamorimark@hotmail.com>
2022-07-19 12:28:33 +0200

    Writing unit tests

--- src/components/MovieCard/__tests__/MovieCard.test.tsx
+++ src/components/MovieCard/__tests__/MovieCard.test.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import { MovieCard } from '../MovieCard';
 
-describe("when rendered with a `releaseDate` prop", () => {
+describe("when rendered with a `releaseDate & title` prop", () => {
   it('renders only the year yyyy (mm/dd sliced)', () => {
     render(<MovieCard title={"Test Movie Title"} overview={"Test Movie Overview"} releaseDate={"2022.02.22"} posterPath={null} favorite={true}/>);
     const textElement = screen.getByText(/2022/i);
     expect(textElement).toBeInTheDocument();
   });
 
-  it('if the prop is an empty string it should be x', () => {
+  it('title should be rendered correctly', () => {
     render(<MovieCard title={"Test Movie Title"} overview={"Test Movie Overview"} releaseDate={""} posterPath={null} favorite={true}/>);
     const h3Element = screen.getByText(/Test Movie Title/i);
     expect(h3Element).toBeInTheDocument();
